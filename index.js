ed93a70b843329a69474db5b0aaef8532aaa0109
paulakedouk <paulakedouk@gmail.com>
2018-02-17 22:49:35 -0800

    Adding files

---
+++ index.js
@@ -0,0 +1,26 @@
+var inquirer = require('inquirer');
+
+// Console a draw to start the game
+// Ask player nickname
+//
+// Create a function that start the game and show the statistics
+
+console.log(`
+000000000000000000000000000
+00000_BY_PAULA_KEDOUK_00000
+    000   000     000      
+    000  000    0000000    
+    0000000    (0000000)   
+    00000       0000000    
+    0000          000      
+    000         0000000    
+    000        00 000 00   
+    000       00  000  00  
+    000      00   000   00 
+    000           000      
+    000         0000000    
+    000       000     000  
+    000      000       000 
+    000      000       000 
+ 000000000                 
+`);
