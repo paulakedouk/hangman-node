423ad2901708a1204d64fa255d6dc14e95653821
paulakedouk <paulakedouk@gmail.com>
2018-02-20 12:00:14 -0800

    Check if letter has guessed before

--- index.js
+++ index.js
@@ -15,41 +15,44 @@ var wordLine = [];
 var rightLetter = [];
 var wrongLetter = [];
 var winCounter = 0;
-
-// Console a draw to start the game
-console.log(`
-000000000000000000000000000
-00000_BY_PAULA_KEDOUK_00000
-000   000     000      
-000  000    0000000    
-000 000    (0000000)   
-00000       0000000    
-0000          000      
-000         0000000    
-000        00 000 00   
-000       00  000  00  
-000      00   000   00 
-000           000      
-000         0000000    
-000       000     000  
-000      000       000 
-000      000       000 
-000000                 
-`);
-
+var userGuesses = [];
 var word = randomWords({ exactly: 1 })[0];
 
-// Show that word
+function startGame() {
+  // Console a draw to start the game
+  console.log(`
+  000000000000000000000000000
+  00000_BY_PAULA_KEDOUK_00000
+  000   000     000      
+  000  000    0000000    
+  000 000    (0000000)   
+  00000       0000000    
+  0000          000      
+  000         0000000    
+  000        00 000 00   
+  000       00  000  00  
+  000      00   000   00 
+  000           000      
+  000         0000000    
+  000       000     000  
+  000      000       000 
+  000      000       000 
+  000000                 
+  `);
 
-for (var i = 0; i < word.length; i++) {
-  wordLine.push('_');
+  // Show that word
+
+  for (var i = 0; i < word.length; i++) {
+    wordLine.push('_');
+  }
+  console.log('________________________________\n');
+  console.log('Word: ' + wordLine.join(' '));
+  console.log(word);
+  guessLetter();
 }
-console.log('________________________________\n');
-console.log('Word: ' + wordLine.join(' '));
-console.log(word);
-game();
+startGame();
 
-function game() {
+function guessLetter() {
   inquirer
     .prompt([
       {
@@ -69,22 +72,22 @@ function game() {
 }
 
 function checkLetter(user) {
-  if (word.indexOf(user.guess) > -1) {
-    for (var i = 0; i < word.length; i++) {
-      if (user.guess === word.charAt(i)) {
-        console.log('\n' + chalk.green('Correct!'));
-        wordLine[i] = user.guess;
-        console.log('Word: ' + wordLine.join(' ') + '\n');
-
-        console.log('________________________________\n');
-        finishGame(word);
-        game();
-      }
-    }
+  if (choices.includes(user.guess) || userGuesses.includes(user.guess)) {
+    console.log("\nYou've chosen that letter before");
+    console.log('________________________________\n');
   } else {
-    if (choices.includes(user.guess)) {
-      console.log("\nYou've chosen that letter before");
-      console.log('________________________________\n');
+    if (word.indexOf(user.guess) > -1) {
+      for (var i = 0; i < word.length; i++) {
+        if (user.guess === word.charAt(i)) {
+          userGuesses.push(user.guess);
+          console.log('\n' + chalk.green('Correct!'));
+          wordLine[i] = user.guess;
+          console.log('Word: ' + wordLine.join(' ') + '\n');
+          winCounter++;
+          console.log('________________________________\n');
+          finishGame(word);
+        }
+      }
     } else {
       choices.push(user.guess);
       console.log('\n' + chalk.red('Wrong!'));
@@ -92,19 +95,42 @@ function checkLetter(user) {
       choicesLeft--;
       console.log('\nChoices left: ' + choicesLeft);
       console.log('________________________________\n');
-    }
 
-    finishGame(word);
-    game();
+      finishGame(word);
+    }
   }
+  guessLetter();
+  finishGame(word);
 }
 
 function finishGame(word) {
-  if (winCounter === word.length) {
+  var finalWord = wordLine.join('').split(',');
+  if (finalWord === word) {
     console.log('\n' + chalk.green('YOU WON!') + '\n');
     console.log('________________________________\n');
+    reset();
+    console.log('\n');
   } else if (choicesLeft === 0) {
     console.log('\n' + chalk.red('GAME OVER!') + '\n');
     console.log('________________________________\n');
+    reset();
+    console.log('\n');
   }
+}
+
+function reset() {
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        message: 'Do you want to start a new game?',
+        name: 'newGame',
+        default: true
+      }
+    ])
+    .then(function(response) {
+      if (response.newGame) {
+        startGame();
+      }
+    });
 }
