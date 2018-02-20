4145553d8b494ee31c9e1fd91432181d51e76fbd
paulakedouk <paulakedouk@gmail.com>
2018-02-20 14:22:06 -0800

    Finish the game when the user no longer wants to play

--- Letter.js
+++ Letter.js
@@ -1,11 +1,70 @@
-function Letter(letter) {
-  this.letter = letter;
-  if (this.character) {
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+var randomWords = require('random-words');
+var word = randomWords({ exactly: 1 })[0];
+var choices = [];
+var userGuesses = [];
+var wordLine = [];
+var winCounter = 0;
+var choicesLeft = 10;
+
+function checkLetter(user) {
+  // Check if the user has already chosen that letter before
+
+  if (choices.includes(user.guess) || userGuesses.includes(user.guess)) {
+    console.log("\nYou've chosen that letter before");
+    console.log('________________________________\n');
+    guessLetter();
+  } else {
+    // If the user guess is greater than -1 it means that the letter exists in that word
+    if (word.indexOf(user.guess) > -1) {
+      for (var i = 0; i < word.length; i++) {
+        // Go through the word array, find the letter and then replace the '_' with that letter
+        if (user.guess === word.charAt(i)) {
+          userGuesses.push(user.guess);
+          // Change color to green
+          console.log('\n' + chalk.green('Correct!'));
+          wordLine[i] = user.guess;
+          // Shows the word with the new letter
+          console.log('Word: ' + wordLine.join(' ') + '\n');
+          winCounter++;
+          console.log('________________________________\n');
+          guessLetter();
+          finishGame(word);
+        }
+      }
+    } else {
+      choices.push(user.guess);
+      console.log('\n' + chalk.red('Incorrect!'));
+      console.log('Chosen letters: ' + choices);
+      choicesLeft--;
+      console.log('\nChoices left: ' + choicesLeft);
+      console.log('________________________________\n');
+      guessLetter();
+      finishGame(word);
+    }
   }
 }
 
-var newLetter = new Letter('A');
+// Function for the user to choose the letter
+function guessLetter() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'guess',
+        message: 'Guess a letter: ',
+        validate: function(str) {
+          if (str.length != 1) return false;
+          var regEx = new RegExp('^[a-zA-Zs]{1,1}$');
+          return regEx.test(str);
+        }
+      }
+    ])
+    .then(function(user) {
+      // Calling function checkLetter() and passing the letter that the user chose as an argument
+      CheckLetter(user);
+    });
+}
 
-console.log(newLetter.letter);
-
-module.exports = Letter;
+module.exports = checkLetter;
--- index.js
+++ index.js
@@ -4,47 +4,44 @@ var randomWords = require('random-words');
 
 // Create a function that start the game and show the statistics
 
-var hasLetter = false;
-var choices = [];
-var shot = 10;
-
-var win = 0;
-var loss = 0;
-var choicesLeft = 10;
-var wordLine = [];
-var rightLetter = [];
-var wrongLetter = [];
 var winCounter = 0;
+var choicesLeft = 10;
+var choices = [];
 var userGuesses = [];
+var wordLine = [];
 var word = randomWords({ exactly: 1 })[0];
+var round = 0;
+var win = 0;
+var lose = 0;
+
+// Console a draw to start the game
+console.log(`
+000000000000000000000000000
+00000_BY_PAULA_KEDOUK_00000
+000   000     000      
+000  000    0000000    
+000 000    (0000000)   
+00000       0000000    
+0000          000      
+000         0000000    
+000        00 000 00   
+000       00  000  00  
+000      00   000   00 
+000           000      
+000         0000000    
+000       000     000  
+000      000       000 
+000      000       000 
+000000                 
+`);
 
 function startGame() {
-  // Console a draw to start the game
-  console.log(`
-  000000000000000000000000000
-  00000_BY_PAULA_KEDOUK_00000
-  000   000     000      
-  000  000    0000000    
-  000 000    (0000000)   
-  00000       0000000    
-  0000          000      
-  000         0000000    
-  000        00 000 00   
-  000       00  000  00  
-  000      00   000   00 
-  000           000      
-  000         0000000    
-  000       000     000  
-  000      000       000 
-  000      000       000 
-  000000                 
-  `);
-
-  // Show that word
-
+  // Shows the number of letters with underline
   for (var i = 0; i < word.length; i++) {
     wordLine.push('_');
   }
+  round++;
+  console.log('Round: ' + round);
   console.log('________________________________\n');
   console.log('Word: ' + wordLine.join(' '));
   console.log(word);
@@ -52,6 +49,7 @@ function startGame() {
 }
 startGame();
 
+// Function for the user to choose the letter
 function guessLetter() {
   inquirer
     .prompt([
@@ -67,58 +65,69 @@ function guessLetter() {
       }
     ])
     .then(function(user) {
+      // Calling function checkLetter() and passing the letter that the user chose as an argument
       checkLetter(user);
     });
 }
 
 function checkLetter(user) {
+  // Check if the user has already chosen that letter before
+
   if (choices.includes(user.guess) || userGuesses.includes(user.guess)) {
     console.log("\nYou've chosen that letter before");
     console.log('________________________________\n');
+    guessLetter();
   } else {
+    // If the user guess is greater than -1 it means that the letter exists in that word
     if (word.indexOf(user.guess) > -1) {
       for (var i = 0; i < word.length; i++) {
+        // Go through the word array, find the letter and then replace the '_' with that letter
         if (user.guess === word.charAt(i)) {
           userGuesses.push(user.guess);
+          // Change color to green
           console.log('\n' + chalk.green('Correct!'));
           wordLine[i] = user.guess;
+          // Shows the word with the new letter
           console.log('Word: ' + wordLine.join(' ') + '\n');
           winCounter++;
           console.log('________________________________\n');
+          guessLetter();
           finishGame(word);
         }
       }
     } else {
       choices.push(user.guess);
-      console.log('\n' + chalk.red('Wrong!'));
+      console.log('\n' + chalk.red('Incorrect!'));
       console.log('Chosen letters: ' + choices);
       choicesLeft--;
       console.log('\nChoices left: ' + choicesLeft);
       console.log('________________________________\n');
-
+      if (choicesLeft > 0) {
+        guessLetter();
+      }
       finishGame(word);
     }
   }
-  guessLetter();
-  finishGame(word);
 }
 
 function finishGame(word) {
-  var finalWord = wordLine.join('').split(',');
-  if (finalWord === word) {
-    console.log('\n' + chalk.green('YOU WON!') + '\n');
+  if (winCounter === word.length) {
+    console.log('\n' + chalk.green('You got it! New word: ') + '\n');
+    win++;
+    console.log('Win: ' + win + ', Lose: ' + lose);
     console.log('________________________________\n');
-    reset();
-    console.log('\n');
+    newRound();
   } else if (choicesLeft === 0) {
-    console.log('\n' + chalk.red('GAME OVER!') + '\n');
+    console.log('The word has: ' + word.toUpperCase());
+    console.log('\n' + chalk.red('Game over! New word: ') + '\n');
     console.log('________________________________\n');
-    reset();
-    console.log('\n');
+    lose++;
+    console.log('Win: ' + win + ', Lose: ' + lose);
+    newRound();
   }
 }
 
-function reset() {
+function newRound() {
   inquirer
     .prompt([
       {
@@ -130,7 +139,22 @@ function reset() {
     ])
     .then(function(response) {
       if (response.newGame) {
+        reset();
         startGame();
+      } else {
+        console.log('See you next time!');
       }
     });
+}
+
+function reset() {
+  winCounter = 0;
+  choicesLeft = 10;
+  choices = [];
+  userGuesses = [];
+  wordLine = [];
+  word = new Word(randomWords({ exactly: 1 })[0]);
+  round = 0;
+  win = 0;
+  lose = 0;
 }
